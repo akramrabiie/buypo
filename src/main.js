3a6ebe5e434a9dd462bec9a9ea7a480ace2647bf
akramrabiie <akramrabiie@gmail.com>
2020-10-09 11:38:46 +0330

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import router from './router'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
